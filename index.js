d714d4ca4fbc477c2f4960486c4a9924cc561fbd
official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-05-21 02:44:18 +0100

    second commit

--- index.js
+++ index.js
@@ -1,56 +1,82 @@
 const swiper1 = new Swiper('.swiper1', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  effect: 'flip',
 
+  autoplay: {
+    delay: 3000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination1',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  slidesPerView: 1,
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination2',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  // slidesPerView: 2,
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+    950: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
     autoplay: {
       delay: 5000,
     },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination1',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    
-    slidesPerView: 2,
-  });
-
-  const swiper2 = new Swiper('.swiper2', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-    effect: 'flip',
-
-    autoplay: {
-      delay: 3000,
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination2',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    
-    slidesPerView: 1,
-  });
+  }
+});
