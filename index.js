0c9dc9e0d662cebb791f99e153749ef299a10350
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-05-24 14:11:35 +0100

    edited

--- index.js
+++ index.js
@@ -81,3 +81,90 @@ const swiper2 = new Swiper('.swiper2', {
   }
 });
 
+const swiper3 = new Swiper('.swiper3', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination2',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  slidesPerView: 1,
+});
+
+const swiper4 = new Swiper('.swiper4', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  effect: 'fade',
+
+  autoplay: {
+    delay: 3000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination4',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  slidesPerView: 1,
+});
+
+const swiper5 = new Swiper('.swiper5', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  effect: 'fade',
+
+  autoplay: {
+    delay: 3000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination5',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  slidesPerView: 1,
+});
+
